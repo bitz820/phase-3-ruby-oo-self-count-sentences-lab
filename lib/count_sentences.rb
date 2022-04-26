91ae19946cbe07515dfb525a130cf0c2b5afb87c
Mark <mark.n.bacon.jr@gmail.com>
2022-04-26 11:27:07 -0400

    commit tree f25fdd2dcf4fbe433d072dcee681065648ce5c28

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    sentences = 0
+    punctuation = ["!", ".", "?"]
+
+    self.chars.each.with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+sentences
+    # individual_words = self.split("." || "?" || "!")
+    # binding.pry
+    # individual_words.delete_if(&:empty?)
+    # individual_words.count
 
   end
 end
